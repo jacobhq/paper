b9728706e4516d094a73bf8e6618e7bd91ed3346
Jacob Marshall <me@jhqcat.com>
2024-11-25 19:52:59 +0000

    fix avatar

--- components/nav-user.tsx
+++ components/nav-user.tsx
@@ -56,7 +56,7 @@ export function NavUser() {
                     className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
                   >
                     <Avatar className="h-8 w-8 rounded-lg">
-                      <AvatarImage src={user.imageUrl} alt={user.fullName as string}/>
+                      <AvatarImage className="object-cover" src={user.imageUrl} alt={user.fullName as string}/>
                       <AvatarFallback className="rounded-lg">{user.firstName as string[0]}</AvatarFallback>
                     </Avatar>
                     <div className="grid flex-1 text-left text-sm leading-tight">
